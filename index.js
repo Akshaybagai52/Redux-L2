ba5ebb9ad0544d28ae143587ff6122f8d3b10589
Akshay Bagai <100753870+Akshaybagai52@users.noreply.github.com>
2023-06-13 01:13:44 +0530

    done

--- index.js
+++ index.js
@@ -77,6 +77,12 @@ const rootReducer = combineReducers({
   iceCream: iceCreamReducer,
 });
 
+const store = createStore(rootReducer);
+console.log("Initial State", store.getState());
+
+const unsubscribe = store.subscribe(() =>
+  console.log("updated state", store.getState())
+);
 
 store.dispatch(orderCake());
 store.dispatch(orderCake());
