b7ca4ce51aec9f8390872e7c7ec2e0176795d8e6
Akshay Bagai <100753870+Akshaybagai52@users.noreply.github.com>
2023-07-03 14:31:51 +0530

    redux-logger

--- redux-toolkit/index.js
+++ redux-toolkit/index.js
@@ -1,14 +1,12 @@
-const store = require('./app/store')
-const { cakeActions } = require('./features/cake/cakeSlice')
-const { icecreamActions } = require('./features/icecream/icecreamSlice')
+const store = require("./app/store");
+const { cakeActions } = require("./features/cake/cakeSlice");
+const { icecreamActions } = require("./features/icecream/icecreamSlice");
 
-console.log('Initial State', store.getState())
-const unsubscribe = store.subscribe(() => {
-    console.log('Updated State', store.getState())
-})
+console.log("Initial State", store.getState());
+const unsubscribe = store.subscribe(() => {});
 
-store.dispatch(icecreamActions.ordered())
-store.dispatch(icecreamActions.ordered())
-store.dispatch(icecreamActions.ordered())
-store.dispatch(icecreamActions.restocked(3))
-unsubscribe()
+store.dispatch(icecreamActions.ordered());
+store.dispatch(icecreamActions.ordered());
+store.dispatch(icecreamActions.ordered());
+store.dispatch(icecreamActions.restocked(3));
+unsubscribe();
