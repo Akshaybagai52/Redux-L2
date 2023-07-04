cf968f61a2e9a46232187b4d31f35d534e4e1afa
Akshay Bagai <100753870+Akshaybagai52@users.noreply.github.com>
2023-07-04 07:42:09 +0530

    provider

--- react-redux/vite-project/src/App.jsx
+++ react-redux/vite-project/src/App.jsx
@@ -1,13 +1,9 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import CakeView from './features/cake/cakeView'
 import IcecreamView from './features/icecream/icecreamView'
 import UserView from './features/user/userView'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
