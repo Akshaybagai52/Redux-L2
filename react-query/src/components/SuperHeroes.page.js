382a76eb51da0fa9e4bec46152997a074c03955e
Akshay Bagai <100753870+Akshaybagai52@users.noreply.github.com>
2023-07-08 09:24:40 +0530

    basic setup

---
+++ react-query/src/components/SuperHeroes.page.js
@@ -0,0 +1,26 @@
+import React, { useEffect, useState } from 'react'
+
+const SuperHeroesPage = () => {
+    const [isLoading, setIsLoading] = useState(true);
+    const [data, setData] = useState([]);
+
+    useEffect(() => {
+        axios.get('http://localhost:400/superheroes').then((res) => {
+            setData(res.data)
+            setIsLoading(false)
+        })
+    }, [])
+    if(isLoading) {
+        return <h2>Loading...</h2>
+    }
+  return (
+    <div>
+        <h2>Super Heroes Page</h2>
+        {data.map((item) => {
+            return <div key={item.name}>{item} </div>
+        })}
+    </div>
+  )
+}
+
+export default SuperHeroesPage
