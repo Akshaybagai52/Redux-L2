05b2aeccd652d4fbad3b86cfb9ea5e5f8264c036
Akshay Bagai <100753870+Akshaybagai52@users.noreply.github.com>
2023-07-16 16:29:06 +0530

    data transformation

--- react-query/src/components/RQSuperHeroes.page.jsx
+++ react-query/src/components/RQSuperHeroes.page.jsx
@@ -3,25 +3,35 @@ import React from 'react'
 import { useQuery } from 'react-query'
 
 const RQSuperHeroesPage = () => {
-  const {isLoading, data, isError, error} = useQuery('super-heroes', () => {
-   return axios.get('http://localhost:400/superheroes')
+  const { isLoading, data, isError, error } = useQuery('super-heroes', () => {
+    return axios.get('http://localhost:400/superheroes')
   }, {
     refetchOnMount: false,
     refetchOnWindowFocus: true,
+    select: (data) => {
+      const superHeroNames = data.data.map(hero => hero.name)
+      return superHeroNames;
+    }
   })
-  if(isLoading) {
+  if (isLoading) {
     return <h2>Loading.....</h2>
   }
-  if(isError) {
+  if (isError) {
     return <h2>{error.message}</h2>
   }
   return (
     <div>RQSuperHeroes.page
-    {
-      data?.data.map((hero) => {
-        return <div key={hero.name}>{hero.name}</div>
-      })
-    }
+      {/* {
+        data?.data.map((hero) => {
+          return <div key={hero.name}>{hero.name}</div>
+        })
+      } */}
+      {
+        data.map((heroName) => {
+          return <div key={heroName}>{heroName} </div>
+        })
+       
+      }
     </div>
   )
 }
